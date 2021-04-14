48408bb2753647370df7941e87f105ed68a7a3b7
Cameron Mastoras <cameroninoz@gmail.com>
2021-04-14 15:06:54 -0500

    HW4 Done

--- HW4/HW4_Cameron_Matoras.X/main.c
+++ HW4/HW4_Cameron_Matoras.X/main.c
@@ -49,12 +49,38 @@ int main() {
     DDPCONbits.JTAGEN = 0;
 
     // do your TRIS and LAT commands here
-
+    TRISAbits.TRISA4 = 0;
+    TRISBbits.TRISB4 = 1;
+    LATAbits.LATA4 = 0;
     __builtin_enable_interrupts();
 
     while (1) {
         // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
         // remember the core timer runs at half the sysclk
-
+        if(PORTBbits.RB4 == 0){
+           
+            
+            _CP0_SET_COUNT(0);
+            while(_CP0_GET_COUNT() < 12000000){
+                ;
+            }
+            LATAbits.LATA4 = 1;
+            _CP0_SET_COUNT(0);
+            while(_CP0_GET_COUNT() < 12000000){
+                ;
+            }
+            LATAbits.LATA4 = 0;
+            _CP0_SET_COUNT(0);
+            while(_CP0_GET_COUNT() < 12000000){
+                ;
+            }
+            LATAbits.LATA4 = 1;
+            _CP0_SET_COUNT(0);
+            while(_CP0_GET_COUNT() < 12000000){
+                ;
+            }
+            LATAbits.LATA4 = 0;}
+        
+        
     }
 }
