5efe770f4663114aaf8d5947e1e90c3319b70b4d
Cameron Mastoras <cameroninoz@gmail.com>
2021-06-08 14:30:36 -0500

    Linguistics stuff I guess

--- LING334/Lyrics Grabber.py
+++ LING334/Lyrics Grabber.py
@@ -15,9 +15,15 @@ client_access_token ='tJEazB1jCEgPTCgXkkqHgvgBJG-vTEdFJfCg0l0SpSSbwBO-TKqRiibvhs
 #api = genius.Genius(client_access_token)
 #artist = genius.search_artist('Action Bronson',max_songs = 1)
 #artist.save_lyrics()
-#white_rappers = ['Eminem','Beastie Boys','Yelawolf','Mac Miller','G-Easy','Macklemore','Machine Gun Kelly','Lil Dicky','Action Bronson', 'NF','Mike Shinoda','House of Pain','R.A. The Rugged Man' ,'Rittz' ,'Brother Ali' ,'Everlast', 'EI-P','Kid Rock', 'Paul Wall','Post Malone','Weird Al Yankovic','Vinnie Paz','Hoodie Allen' ,'Asher Roth' ]
-black_rappers = ['Ice cube','Nas','Kendrick Lamar','Tupac','The Notorious B.I.G.','J. Cole','Snoop Dogg','Lil Wayne','Jay-Z','Kayne West','André 3000','50 Cent','Eazy-E','MF DOOM','Travis Scott','Kid Cudi','ASAP Rocky','Nicki Minaj','DMX','Tyler, The Creator','Childish Gambino','Logic','Nate Dogg','Joey Bada$$']
-artist_list = ["Saba"]
+# white_rappers = ['Eminem','Beastie Boys','Yelawolf','Mac Miller','G-Easy','Macklemore','Machine Gun Kelly','Lil Dicky','Action Bronson', 'NF','Mike Shinoda','House of Pain','R.A. The Rugged Man'] 
+# white_rappers2 = ['Rittz' ,'Brother Ali' ,'Everlast', 'Token','Kid Rock', 'Paul Wall','Post Malone','Weird Al Yankovic','Vinnie Paz','Hoodie Allen' ,'Asher Roth' ]
+
+# black_rappers3 = ['Ice cube','Nas','Kendrick Lamar','2pac','The Notorious B.I.G.','J. Cole','Snoop Dogg','Lil Wayne','Jay-Z','Kayne West','Gucci Mane','50 Cent','Eazy-E','MF DOOM','Travis Scott']
+# black_rappers2 = ['Kid Cudi','ASAP Rocky','Nicki Minaj','DMX','Tyler, The Creator','Childish Gambino','Logic','Nate Dogg','Joey Bada$$']
+
+black_rappers = ['Nate Dogg','Snoop Dogg','Ice cube','Nas','Kendrick Lamar','2pac','J. Cole','Lil Wayne','Jay-Z','Kayne West','Gucci Mane','50 Cent','Eazy-E','MF DOOM','Travis Scott','Kid Cudi','ASAP Rocky','Nicki Minaj','DMX','Tyler, The Creator','Childish Gambino','Logic','Joey Bada$$']
+
+artist_list = ['Ice cube','Nas','Kendrick Lamar','2pac','The Notorious B.I.G.']
 num_songs = 10
 
 songs = genius.search_songs('Baby Blue')
@@ -73,32 +79,39 @@ class Song:
         self.lyrics = self.lyrics.replace("("," ")
         self.lyrics = self.lyrics.replace(")"," ")
         self.lines = list(filter(None,re.split("[\r\n]|\[.*\]",self.lyrics)))
-        print(self.lines)
+        #print(self.lines)
     def tokenize(self):
         print("HELLOW")
 
 
 def lyric_grabber2(artistname,numsongs,race):
+    try:
+        artist = genius.search_artist(artistname, max_songs=numsongs)
     
-    artist = genius.search_artist(artistname, max_songs=numsongs)
-    artist.save_lyrics()
-    parsedname = artistname.replace(" ","")
-    parsedfilename = "Lyrics_"+str(parsedname)+".json"
-    #Artistread=pd.read_json("Lyrics_ActionBronson.json")
-    
-    with open(parsedfilename, 'r') as myfile:
-        data=myfile.read()
-    #json_obj = json.loads("Lyrics_ActionBronson.json")
-    json_obj = json.loads(data)
-    lyric_list = []
-    for i in range(0,numsongs):
-        date = re.split("-",json_obj['songs'][i]['release_date'])
-        lYrics = json_obj['songs'][i]['lyrics']
-        #print(lYrics)
-        newsong = Song(date[0],race,lYrics)
-        newsong.line_ize()
-        TOTAL_SONG_LIST.append(newsong)
-    print("added all "+str(numsongs)+" "+str(artistname)+" songs to the master list")
+        artist.save_lyrics()
+        parsedname = artistname.replace(" ","")
+        parsedname = artistname.replace("$","")
+        parsedfilename = "Lyrics_"+str(parsedname)+".json"
+        #Artistread=pd.read_json("Lyrics_ActionBronson.json")
+        parsedfilename = parsedfilename.replace("-","")
+        
+        parsedfilename = parsedfilename.replace(" ","")
+        with open(parsedfilename, 'r',encoding="utf-8") as myfile:
+            data=myfile.read()
+        
+        #json_obj = json.loads("Lyrics_ActionBronson.json")
+        json_obj = json.loads(data)
+        lyric_list = []
+        for i in range(0,numsongs):
+            date = re.split("-",json_obj['songs'][i]['release_date'])
+            lYrics = json_obj['songs'][i]['lyrics']
+            #print(lYrics)
+            newsong = Song(date[0],race,lYrics)
+            newsong.line_ize()
+            TOTAL_SONG_LIST.append(newsong)
+        print("added all "+str(numsongs)+" "+str(artistname)+" songs to the master list")
+    except:
+        print("Aww shucks")
   
 def artist_runner(artistlist,numberofsongs):
     print("what race is "+str(artistlist[0]))
@@ -115,16 +128,42 @@ def TOTAL_SONG_COMPILER(artistlist):
 
 biglist = []
 
-def CSV_Saver(songlist):
+def CSV_Saver(songlist,title):
+    
     for song in songlist:
         for line in song.lines:
-            listhing = [song.race,song.year,line]
+            var = line.replace('\u200b','')
+            var = line.replace('\u0435','')
+            
+            listhing = [song.race,song.year,var]
             biglist.append(listhing)
-    np.savetxt("Thisthing1.csv", 
-           biglist,
-           delimiter =", ", 
-           fmt ='% s')
+    with open('100songs2.json', 'w',encoding="utf-8") as f:
+        json.dump(biglist, f)
 
-artist_runner(artist_list,1)
+
+    # np.savetxt(title, 
+    #        biglist,
+    #        delimiter =", ", 
+    #        fmt ='% s')
+
+    
+# artist_runner(black_rappers,2)
+# CSV_Saver(TOTAL_SONG_LIST,"B1.csv")
+# TOTAL_SONG_LIST = []
+# artist_runner(black_rappers2,2)
+# CSV_Saver(TOTAL_SONG_LIST,"B2.csv")
+# TOTAL_SONG_LIST = []
+# artist_runner(white_rappers,2)
+# CSV_Saver(TOTAL_SONG_LIST,"W1.csv")
+# TOTAL_SONG_LIST = []
+# artist_runner(white_rappers2,2)
+# CSV_Saver(TOTAL_SONG_LIST,"W2.csv")
+# TOTAL_SONG_LIST = []
+
+artist_runner(black_rappers,2)
+CSV_Saver(TOTAL_SONG_LIST,"Test.csv")
+
+#artist_runner(black_rappers,2)
+#artist_runner(artist_list,2)
 #print(TOTAL_SONG_LIST[1].lyrics)
-CSV_Saver(TOTAL_SONG_LIST)
+#CSV_Saver(TOTAL_SONG_LIST,"Test.csv")
